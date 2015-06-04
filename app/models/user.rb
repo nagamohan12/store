ba657c44b80d985f6520b77244e02a28a51bec2d
nagamohan12 <nagamohan2014@gmail.com>
2015-06-04 14:21:15 +0530

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+# User Model
+class User < ActiveRecord::Base
+  rolify
+  # before_action :authenticate_user!
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
